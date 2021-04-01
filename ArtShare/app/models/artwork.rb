3872cdb9e867c8f26e91acafe44190b318cbf2b6
Mohammad Taher <zohebtaher@gmail.com>
2021-04-01 10:22:45 -0400

    Added associations

--- ArtShare/app/models/artwork.rb
+++ ArtShare/app/models/artwork.rb
@@ -1,9 +1,19 @@
 class Artwork < ApplicationRecord
     validates :title, :image_url, :artist_id, presence: true
-    validates :artist_id, uniqueness: {scope: :title}
+    validates :title, uniqueness: {scope: :artist_id}
 
     belongs_to :artist,
         primary_key: :id,
         foreign_key: :artist_id,
         class_name: :User
+
+    has_many :viewers,
+        primary_key: :id,
+        foreign_key: :artwork_id,
+        class_name: :ArtworkShare
+
+        has_many :shared_viewers,
+            through: :viewers
+            source: :viewer
+
 end
--- ArtShare/app/models/user.rb
+++ ArtShare/app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
     validates :username,  presence: true, uniqueness: true
+
+
+    has_many :views,
+        primary_key: :id,
+        foreign_key: :viewer_id,
+        class_name: :ArtworkShare
+
+    has_many :shared_artworks,
+        through: :views,
+        source: :artwork
 end
