c574d1875cd6e43193bf9bae925d9d8a76ce242d
Mohammad Taher <zohebtaher@gmail.com>
2021-04-01 10:39:44 -0400

    Added a few more associations

--- ArtShare/app/models/user.rb
+++ ArtShare/app/models/user.rb
@@ -2,12 +2,17 @@ class User < ApplicationRecord
     validates :username,  presence: true, uniqueness: true
 
 
-    has_many :views,
+    has_many :artworks,
+        primary_key: :id,
+        foreign_key: :artist_id
+        class_name: :Artwork
+
+    has_many :artwork_shares,
         primary_key: :id,
         foreign_key: :viewer_id,
         class_name: :ArtworkShare
 
     has_many :shared_artworks,
-        through: :views,
+        through: :artwork_shares,
         source: :artwork
 end
